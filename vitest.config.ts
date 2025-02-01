2564db3fc70f538762651a03371584c62f62ea20
pawcode Development <development@pawcode.de>
2025-02-01 12:00:41 +0100

    ci(test): update test actions

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   test: {
-    include: ["test/**/*.spec.ts"],
-    silent: true
+    include: ["test/**/*.spec.ts", "test/**/*.e2e-spec.ts"],
+    silent: true,
+    coverage: {
+      include: ["src/**/*.ts"]
+    }
   }
 });
