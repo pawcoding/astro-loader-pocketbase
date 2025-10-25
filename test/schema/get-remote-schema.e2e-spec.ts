b2741bc342f46f82b81ce6440731d290df82eb90
pawcode Development <development@pawcode.de>
2025-10-25 18:45:13 +0200

    test(schema): simplify test case

--- test/schema/get-remote-schema.e2e-spec.ts
+++ test/schema/get-remote-schema.e2e-spec.ts
@@ -35,28 +35,6 @@ describe("getRemoteSchema", () => {
 
     assert(result, "Schema is not defined.");
 
-    // Delete unstable properties
-    // @ts-expect-error - created is not typed
-    delete result.created;
-    // @ts-expect-error - updated is not typed
-    delete result.updated;
-    for (const field of result.fields) {
-      // @ts-expect-error - id is not typed
-      delete field.id;
-    }
-
-    // Delete email templates
-    // @ts-expect-error - authAlert is not typed
-    delete result.authAlert;
-    // @ts-expect-error - otp is not typed
-    delete result.otp;
-    // @ts-expect-error - verificationTemplate is not typed
-    delete result.verificationTemplate;
-    // @ts-expect-error - resetPasswordTemplate is not typed
-    delete result.resetPasswordTemplate;
-    // @ts-expect-error - confirmEmailChangeTemplate is not typed
-    delete result.confirmEmailChangeTemplate;
-
     expect(result).toMatchSnapshot();
   });
 });
