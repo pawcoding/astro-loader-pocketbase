dab3c82773402bb01c8be69e0fa08e72395f7c80
pawcode Development <development@pawcode.de>
2026-03-16 14:18:23 +0100

    fix(schema): get type generation working for collections with files again
    This was only broken in the Astro v6 release candidates.

--- src/schema/generate-type.ts
+++ src/schema/generate-type.ts
@@ -1,4 +1,4 @@
-import type { ZodType } from "astro/zod";
+import type { ZodObject, ZodPipe } from "astro/zod";
 import {
   createAuxiliaryTypeStore,
   createTypeAlias,
@@ -9,8 +9,12 @@ import {
 /**
  * Generate a TypeScript type from a Zod schema.
  */
-export function generateType(schema: ZodType): string {
-  const { node } = zodToTs(schema, {
+export function generateType(schema: ZodObject | ZodPipe): string {
+  // If the collection contains file fields, we transform the field values to file urls in a `transform` step.
+  // This means that the schema for the types is different from the schema for the data, so we need to extract the inner schema for the types.
+  const schemaForTypes = schema.type === "pipe" ? schema.in : schema;
+
+  const { node } = zodToTs(schemaForTypes, {
     auxiliaryTypeStore: createAuxiliaryTypeStore()
   });
   const typeAlias = createTypeAlias(node, "Entry");
